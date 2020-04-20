4716fa8e313bc2d9bccc9c1b3620ecab7009ee76
Basile <vandervalle.basile@gmail.com>
2020-04-20 20:08:07 +0200

    optimisation de fabrication de graph

--- src/Autre/FabriquerGraph.java
+++ src/Autre/FabriquerGraph.java
@@ -6,23 +6,19 @@ import Type.Sommet;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class FabriquerGraph {
 	private Graphe graphe;
 
     public FabriquerGraph(String NomFichier) throws IOException{
     	Graphe graphe = new Graphe();
-        List<Sommet> listSommet = new ArrayList<Sommet>();
 
         BufferedReader in = new BufferedReader(new FileReader("src/Graphe/" + NomFichier));
         String line;
         String[] Sommet;
         int nbS = 0;
-        int Sommettrouver = 0;
-        boolean cont = true;
-        int k;
+        int a;
+        int b;
 
         //Connaitre le nombre total de sommet
         line = in.readLine();
@@ -36,35 +32,20 @@ public class FabriquerGraph {
         Sommet[] tabSommet = new Sommet[nbS+1];
         for(int i=1; i<=nbS; i++){
             Sommet sommet = new Sommet(i);
-            graphe.ajoutSommet(sommet);
-            listSommet.add(sommet);
-            tabSommet[i] = sommet;
+            graphe.ajoutSommet(tabSommet[i] = sommet);
         }
 
         //Ajout des sommets au graph
         BufferedReader file = new BufferedReader(new FileReader("src/Graphe/" + NomFichier));
         while ((line = file.readLine()) != null){
             //Si la ligne contient un arc
-            if(line.contains("e")){
-                Sommet = line.split(" ");
-                //Si la premiière lettre est bien un e
-                if(Sommet[0].contains("e")){
-                    for (int i=1; i<=tabSommet.length; i++) {
-                        if(Integer.parseInt(Sommet[1]) == tabSommet[i].getId()){Sommettrouver = i; break;}
-                    }
-                    cont = true;
-                    k = 1;
-                    //Création de arc et ajout voisins
-                    while((k <= tabSommet.length) && cont){
-                        //Recherche dans le tableau de sommet la coreespondace
-                        if(Integer.parseInt(Sommet[2]) == tabSommet[k].getId()){
-                            tabSommet[Sommettrouver].ajouterVoisin(tabSommet[k]);
-                            tabSommet[k].ajouterVoisin(tabSommet[Sommettrouver]);
-                            cont = false;
-                        }
-                        k++;
-                    }
-                }
+            Sommet = line.split(" ");
+            if(Sommet[0].contains("e")){
+            	a = Integer.parseInt(Sommet[1]);
+            	b = Integer.parseInt(Sommet[2]);
+                //Création de arc et ajout voisins
+                tabSommet[a].ajouterVoisin(tabSommet[b]);
+                tabSommet[b].ajouterVoisin(tabSommet[a]);
             }
         }
         file.close();
